dfbbd8cb5f16e50459d6d775ae61885571d0c6cb
JJ <infernoj2581@gmail.com>
2018-02-09 08:46:57 -0700

    Created the map as a 2d Array

--- src/game/model/Room.java
+++ src/game/model/Room.java
@@ -1,5 +1,11 @@
 package game.model;
 
 public class Room {
-
+	String name;
+	int number;
+	public Room(String name, int number) 
+	{
+		this.name = name;
+		this.number = number;
+	}
 }
--- src/game/view/gamePanel.java
+++ src/game/view/gamePanel.java
@@ -6,9 +6,28 @@ import game.controller.*;
 public class gamePanel extends JPanel
 {
 	gameController controller;
+	
+	JTextField input;
+	
 	public gamePanel(gameController controller) 
 	{
 		super();
 		this.controller = controller;
+	}
+	private void setupPanel() 
+	{
+		input = new JTextField();
+	}
+	private void setupLayout() 
+	{
+		
+	}
+	private void setupListeners() 
+	{
+		
+	}
+	public String userInput() 
+	{
+		return input.getText();
 	}
 }
